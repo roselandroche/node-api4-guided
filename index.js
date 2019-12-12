0cd9d60c481f2eebec4fd51596e993a70b344259
roselandroche <r.landroche@gmail.com>
2019-12-11 20:48:05 -0500

    Following lecture

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require("express")
+// const dotenv = require("dotenv")
+
+// dotenv.config()
 
 const app = express()
 const host = "0.0.0.0"
@@ -12,6 +15,7 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
 	res.json({
 		message: "Welcome to our API",
+		cohort: process.env.LAMBDA_COHORT
 	})
 })
 
