064ee5e3821dd2ddfcf06979f416219e36cdd414
roselandroche <r.landroche@gmail.com>
2019-12-11 21:05:09 -0500

    Following along

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require("express")
 // dotenv.config()
 
 const app = express()
-const host = "0.0.0.0"
-const port = 8080
+const host = process.env.HOST || "0.0.0.0"
+const port = process.env.PORT || 8080
 
 app.use((req, res, next) => {
 	console.log(`[${new Date().toLocaleString()}] ${req.ip} ${req.method} ${req.url}`)
